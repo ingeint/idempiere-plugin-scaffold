f54e27b1b5378a7baffd0fc527c6da9ec9c10933
Saúl Piña <sauljabin@gmail.com>
2024-03-05 19:51:21 -0500

    Add process test

--- example-plugin/com.ingeint.example.test/src/com/ingeint/example/process/PPrintPluginInfoTest.java
+++ example-plugin/com.ingeint.example.test/src/com/ingeint/example/process/PPrintPluginInfoTest.java
@@ -2,14 +2,22 @@ package com.ingeint.example.process;
 
 import static com.ingeint.example.test.assertion.Annotations.assertClassAnnotation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.adempiere.base.annotation.Process;
 import org.junit.jupiter.api.Test;
 
 public class PPrintPluginInfoTest {
-	
+
 	@Test
 	public void containsTheAnnotation() {
 		assertClassAnnotation(PPrintPluginInfo.class, Process.class);
+	}
+
+	@Test
+	public void getPluginInformation() throws Exception {
+		PPrintPluginInfo process = new PPrintPluginInfo();
+		assertThat(process.doIt()).contains("Plugin: Example Plugin");
 	}
 
 }
