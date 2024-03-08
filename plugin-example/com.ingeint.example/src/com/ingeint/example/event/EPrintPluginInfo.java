79b7d1a5aa3c4cef5cdd95b9a7923c911220a56e
Saúl Piña <sauljabin@gmail.com>
2024-03-07 20:56:56 -0500

    using invoce instead

--- plugin-example/com.ingeint.example/src/com/ingeint/example/event/EPrintPluginInfo.java
+++ plugin-example/com.ingeint.example/src/com/ingeint/example/event/EPrintPluginInfo.java
@@ -22,7 +22,7 @@ import org.adempiere.base.annotation.EventTopicDelegate;
 import org.adempiere.base.annotation.ModelEventTopic;
 import org.adempiere.base.event.annotations.ModelEventDelegate;
 import org.adempiere.base.event.annotations.doc.AfterComplete;
-import org.compiere.model.MOrder;
+import org.compiere.model.MInvoice;
 import org.compiere.util.CLogger;
 import org.osgi.service.event.Event;
 
@@ -33,18 +33,18 @@ import com.ingeint.example.base.BundleInfo;
  * https://wiki.idempiere.org/en/NF9_OSGi_New_Event_Handling_Annotation
  */
 @EventTopicDelegate
-@ModelEventTopic(modelClass = MOrder.class)
-public class EPrintPluginInfo extends ModelEventDelegate<MOrder> {
+@ModelEventTopic(modelClass = MInvoice.class)
+public class EPrintPluginInfo extends ModelEventDelegate<MInvoice> {
 
 	private final static CLogger log = CLogger.getCLogger(EPrintPluginInfo.class);
 
-	public EPrintPluginInfo(MOrder po, Event event) {
+	public EPrintPluginInfo(MInvoice po, Event event) {
 		super(po, event);
 	}
 
 	@AfterComplete
 	protected void doAfterComplete() {
-		MOrder docTemplate = getModel();
+		MInvoice docTemplate = getModel();
 		log.info(docTemplate.toString());
 		log.info("Plugin: %s, Plugin ID: %s".formatted(BundleInfo.getBundleName(), BundleInfo.getBundleID()));
 	}
